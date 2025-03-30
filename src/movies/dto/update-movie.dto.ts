6f0d9aa6fe84dddd2f89c1db93edd3321ecfca12
Robbie <kdjun1234@gmail.com>
2025-03-30 21:03:00 +0900

    test1

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -3,5 +3,4 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateMovieDto } from "./create-movie.dto";
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {
-    
 }
